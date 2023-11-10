ce17d5147da25860e753eb5cc14e6a11167800d3
IbrahimPV <102571916+IbrahimPV@users.noreply.github.com>
2023-11-10 12:29:01 +0400

    Update user.java

--- user.java
+++ user.java
@@ -2,16 +2,16 @@ import javax.swing.*;
 
 public class user {
     JFrame frame = new JFrame();
-    JLabel signUp = new JLabel("SignUp");
+    JLabel signUpLabel = new JLabel("SignUp");
+    JButton signUp = new JButton("SignUp");
 
     public user() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(350, 580);
         frame.setLayout(null);
 		frame.setVisible(true);
-
-
-
+        signUp.setBounds(175,350,100,50);
+        frame.add(signUp);
 
     }
     
