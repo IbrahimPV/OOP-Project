3631450e7a1a3beac8625b59000187cacc51ff7a
mmakuf <150247525+mmakuf@users.noreply.github.com>
2023-11-10 09:29:17 +0400

    Update COP28.java

--- COP28.java
+++ COP28.java
@@ -1,5 +1,6 @@
 public class COP28 {
     public static void main(String[] args) {
+        System.out.println("hello");
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hi");
