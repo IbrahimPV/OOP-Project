6addb1415867c27a4e7fa60510c5605eba213132
mmakuf <150247525+mmakuf@users.noreply.github.com>
2023-11-10 21:55:56 +0400

    Update user.java

--- user.java
+++ user.java
@@ -1,16 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class user extends JFrame{
     
     JLabel LoginLabel = new JLabel("Log-in");
     JButton Login = new JButton("Log-in");
+    JLabel un = new JLabel("     Enter Username: ", SwingConstants.LEFT);
+    JLabel pas = new JLabel("     Enter Password: ", SwingConstants.LEFT);
+    JTextField unf = new JTextField();
+    JTextField pasf = new JTextField();
+
 
     public user() {
+        setLayout(new GridLayout(10,4));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(350, 580);
-        setLayout(null);
 		setVisible(true);
-        Login.setBounds(175,350,100,50);
+        //Login.setBounds(175,350,100,50);
+        add(new JLabel("Users Login Page",SwingConstants.CENTER));
+        add(new JLabel(" "));
+        add(new JLabel(" "));
+        add(new JLabel(" "));
+        add(un);
+        add(unf);
+        add(new JLabel(" "));
+		add(new JLabel(" "));
+        add(pas);
+        add(pasf);
+		add(new JLabel(" "));
+		add(new JLabel(" "));
+		add(new JLabel(" "));
+       // add(Login);
+        add(new JLabel(" "));
+        add(new JLabel(" "));
+        add(new JLabel(" "));
+        add(new JLabel(" "));
         add(Login);
 
     }
