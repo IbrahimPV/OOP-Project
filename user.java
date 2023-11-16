035f4a20f3f99d99441fc15af2403ce11f00d5c0
IbrahimPV <102571916+IbrahimPV@users.noreply.github.com>
2023-11-16 13:44:18 +0400

    update

--- user.java
+++ user.java
@@ -2,7 +2,89 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class user extends JFrame{
+public class user {
+    private String name, password, email, phoneNumber, day, month, year, address, ID;
+
+    public user(String name, String password, String email, String phoneNumber, String day, String month, String year,
+            String address) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String IDGenerator() {
+        return Double.toString(Math.floor(Math.random()));
+
+    }
+    
    
     
 }
