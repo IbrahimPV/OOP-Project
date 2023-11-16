b0c912fe2e0add84658d0faab697f7ba8178a420
3bdulla <abdullahalhouqani@gmail.com>
2023-11-16 18:12:34 +0400

    Update AdminMain.java

--- AdminMain.java
+++ AdminMain.java
@@ -1,4 +1,21 @@
 //GUI for Admin Main page
-public class AdminMain {
+import java.awt.*;
+import javax.swing.*;
+
+public class AdminMain extends JFrame {
+	
+	JLabel welcome = new JLabel("Welcome");
+	JLabel actvEve = new JLabel("Active Events");
+	
+	
+	
+	public AdminMain() {
+		
+		setLayout(new GridLayout(17,4));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(350, 580);
+        setVisible(true);
+        
+	}
 
 }
