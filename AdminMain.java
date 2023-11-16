72987b82f76a66d5641877bb64a04b604d366cc3
3bdulla <abdullahalhouqani@gmail.com>
2023-11-16 18:35:30 +0400

    a

--- AdminMain.java
+++ AdminMain.java
@@ -5,16 +5,21 @@ import javax.swing.*;
 public class AdminMain extends JFrame {
 	
 	JLabel welcome = new JLabel("Welcome");
+	JLabel name = new JLabel("");
 	JLabel actvEve = new JLabel("Active Events");
-	
+	JTabbedPane activeEvents = new JTabbedPane();
+	JLabel pendEve = new JLabel("Pending Events");
+	JTabbedPane pendingEvents = new JTabbedPane();
 	
 	
 	public AdminMain() {
 		
-		setLayout(new GridLayout(17,4));
+		setLayout(new GridLayout(17,2));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(350, 580);
         setVisible(true);
+        
+        add(new JLabel());
         
 	}
 
