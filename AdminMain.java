c2d936a625ed4d38da3867faf9660eaf10ffc848
3bdulla <abdullahalhouqani@gmail.com>
2023-11-18 10:20:01 +0400

    Update AdminMain.java

--- AdminMain.java
+++ AdminMain.java
@@ -7,11 +7,10 @@ public class AdminMain extends JFrame {
 	JLabel welcome = new JLabel("Welcome");
 	JLabel name = new JLabel("");
 	JLabel actvEve = new JLabel("Active Events");
-	JTabbedPane activeEvents = new JTabbedPane();
-	JLabel event1, event2, event3 = new JLabel();
 	JLabel pendEve = new JLabel("Pending Events");
-	JTabbedPane pendingEvents = new JTabbedPane();
-	JLabel pevent1, pevent2, pevent3 = new JLabel();
+	JTable activeEvents = new JTable();
+	JTable pendingEvents = new JTable();
+	
 	
 	
 	public AdminMain() {
@@ -20,14 +19,7 @@ public class AdminMain extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(350, 580);
         setVisible(true);
-        
-        activeEvents.add(event1);
-        activeEvents.add(event2);    
-        activeEvents.add(event3);    
-        
-        pendingEvents.add(pevent1);    
-        pendingEvents.add(pevent2);    
-        pendingEvents.add(pevent3);    
+   
         
         add(welcome);
         add(name);
