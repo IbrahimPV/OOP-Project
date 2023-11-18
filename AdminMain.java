26989ea1896a8105959bfee7110b2a03683c69ed
3bdulla <abdullahalhouqani@gmail.com>
2023-11-18 16:33:49 +0400

    Update AdminMain.java

--- AdminMain.java
+++ AdminMain.java
@@ -9,33 +9,41 @@ public class AdminMain extends JFrame {
 	JLabel actvEve = new JLabel("	Active Events");
 	JLabel pendEve = new JLabel("	Pending Events");
 	
-	String [] columnNames = {"Event name", "Event type", "date"};
-	Object [][] data = {{"Cleaning beaches", "evniromental cleaning", "12,12,2023"},
-			{"Cleaning beaches", "evniromental cleaning", "12,12,2023"},
-			{"Cleaning beaches", "evniromental cleaning", "12,12,2023"}};
+	String [] columnNames = {"Event name", "Event type", "date", "full"};
+	Object [][] data = {{"Cleaning beaches", "evniromental cleaning", "12,12,2023", true},
+			{"Cleaning parks", "evniromental cleaning", "12,12,2023", false},
+			{"Recycling Plastic", "Recycling", "12,12,2023", false},
+			{"Raising Awarness", "Educational ", "12,12,2023", true},
+			{"Cleaning beaches", "evniromental cleaning", "12,12,2023", false}};
 	
 	JTable activeEvents = new JTable(data, columnNames);
 	JTable pendingEvents = new JTable(data, columnNames);
 	
+	JScrollPane  scroll = new JScrollPane(activeEvents); 
+	JScrollPane  scroll2 = new JScrollPane(pendingEvents);
 	
 	
-	public AdminMain() {
+	
+	public AdminMain() { 
         
 		add(welcome);
         add(name);
         add(new JLabel());
         add(new JLabel());
         add(actvEve);
-        activeEvents.getTableHeader().setBounds(50,0,700,50);
-    	activeEvents.setBounds(50,50,700,200);
-    	add(activeEvents.getTableHeader());
-    	add(activeEvents);
+        //activeEvents.getTableHeader().setBounds(50,0,700,50);
+    	//activeEvents.setBounds(50,50,700,20);
+    	//add(activeEvents.getTableHeader(), BorderLayout.PAGE_START);
+    	//add(activeEvents, BorderLayout.CENTER);
+    	scroll.setBounds(50,50,700,70);
+        add(scroll);
         add(new JLabel());
         add(pendEve);
         pendingEvents.getTableHeader().setBounds(50,0,700,50);
     	pendingEvents.setBounds(50,50,700,200);
-    	add(pendingEvents.getTableHeader());
-        add(pendingEvents);
+    	add(pendingEvents.getTableHeader( ), BorderLayout.PAGE_START);
+        add(pendingEvents, BorderLayout.CENTER);
+        
         
        
         
