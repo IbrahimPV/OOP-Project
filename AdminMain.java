6eb70dee1028f44558a47cc2bc65c952aacf56ac
3bdulla <abdullahalhouqani@gmail.com>
2023-11-18 17:23:02 +0400

    a

--- AdminMain.java
+++ AdminMain.java
@@ -1,6 +1,8 @@
 //GUI for Admin Main page
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.*;
 
 public class AdminMain extends JFrame {
 	
@@ -9,15 +11,22 @@ public class AdminMain extends JFrame {
 	JLabel actvEve = new JLabel("	Active Events");
 	JLabel pendEve = new JLabel("	Pending Events");
 	
-	String [] columnNames = {"Event name", "Event type", "date", "full"};
-	Object [][] data = {{"Cleaning beaches", "evniromental cleaning", "12,12,2023", true},
+	String [] columnNames = {"Event name", "Event type", "date", "full", "See details"};
+	Object [][] data = {{"Cleaning beaches", "evniromental cleaning", "12,12,2023", true, new JButton("Enter")},
+			{"Cleaning parks", "evniromental cleaning", "12,12,2023", false, new JButton("Enter")},
+			{"Recycling Plastic", "Recycling", "12,12,2023", false, new JButton("Enter")},
+			{"Raising Awarness", "Educational ", "12,12,2023", true, new JButton("Enter")},
+			{"Cleaning beaches", "evniromental cleaning", "12,12,2023", false, new JButton("Enter")}};
+	
+	String [] columnNames1 = {"Event name", "Event type", "date", "full"};
+	Object [][] data1 = {{"Cleaning beaches", "evniromental cleaning", "12,12,2023", true},
 			{"Cleaning parks", "evniromental cleaning", "12,12,2023", false},
 			{"Recycling Plastic", "Recycling", "12,12,2023", false},
 			{"Raising Awarness", "Educational ", "12,12,2023", true},
 			{"Cleaning beaches", "evniromental cleaning", "12,12,2023", false}};
 	
 	JTable activeEvents = new JTable(data, columnNames);
-	JTable pendingEvents = new JTable(data, columnNames);
+	JTable pendingEvents = new JTable(data1, columnNames1);
 	
 	JScrollPane  scroll = new JScrollPane(activeEvents); 
 	JScrollPane  scroll2 = new JScrollPane(pendingEvents);
@@ -25,6 +34,8 @@ public class AdminMain extends JFrame {
 	
 	
 	public AdminMain() { 
+		
+		super("Admin Main Page");
         
 		add(welcome);
         add(name);
@@ -35,7 +46,7 @@ public class AdminMain extends JFrame {
     	//activeEvents.setBounds(50,50,700,20);
     	//add(activeEvents.getTableHeader(), BorderLayout.PAGE_START);
     	//add(activeEvents, BorderLayout.CENTER);
-    	scroll.setBounds(50,50,700,70);
+    	scroll.setBounds(60,60,700,70);
         add(scroll);
         add(new JLabel());
         add(pendEve);
@@ -43,13 +54,13 @@ public class AdminMain extends JFrame {
     	//pendingEvents.setBounds(50,50,700,200);
     	//add(pendingEvents.getTableHeader( ), BorderLayout.PAGE_START);
         //add(pendingEvents, BorderLayout.CENTER);
-        scroll2.setBounds(50,50,700,70);
+        scroll2.setBounds(60,60,700,70);
         add(scroll2);
         
         
        
         
-        setLayout(new GridLayout(17,4));
+        setLayout(new GridLayout(17,6));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(700, 900);
         setVisible(true);
@@ -57,7 +68,86 @@ public class AdminMain extends JFrame {
 	}
 	
 	public static void main(String [] args) {
-		new AdminMain();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new AdminMain();
+			}
+			});
+		}
 	}
 
+
+
+
+
+class ButtonRenderer extends JButton implements TableCellRenderer {
+
+    public ButtonRenderer() {
+        setOpaque(true);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+        if (isSelected) {
+            setForeground(table.getSelectionForeground());
+            setBackground(table.getSelectionBackground());
+        } else {
+            setForeground(table.getForeground());
+            setBackground(UIManager.getColor("Button.background"));
+        }
+        setText((value == null) ? "" : value.toString());
+        return this;
+    }
+}
+
+class ButtonEditor extends DefaultCellEditor {
+
+    protected JButton button;
+    private String label;
+    private boolean isPushed;
+
+    public ButtonEditor(JCheckBox checkBox) {
+        super(checkBox);
+        button = new JButton();
+        button.setOpaque(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fireEditingStopped();
+            }
+        });
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value,
+            boolean isSelected, int row, int column) {
+        if (isSelected) {
+            button.setForeground(table.getSelectionForeground());
+            button.setBackground(table.getSelectionBackground());
+        } else {
+            button.setForeground(table.getForeground());
+            button.setBackground(table.getBackground());
+        }
+        label = (value == null) ? "" : value.toString();
+        button.setText(label);
+        isPushed = true;
+        return button;
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        if (isPushed) {
+            JOptionPane.showMessageDialog(button, label + ": Ouch!");
+        }
+        isPushed = false;
+        return label;
+    }
+
+    @Override
+    public boolean stopCellEditing() {
+        isPushed = false;
+        return super.stopCellEditing();
+    }
 }
